1b8968be8fa69f699ff83109a0b86005e660705b
redmine <ekyastrebova@mail.ru>
2018-10-19 13:40:05 +0300

    php "current" fixed

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ function viewInit($file, $arr = [])
     $file = $file . '.html';
     include "views/$file";
 }
-viewInit('header');
+viewInit('header', array('current' => 'index'));
 viewInit('index');
 viewInit('footer');
